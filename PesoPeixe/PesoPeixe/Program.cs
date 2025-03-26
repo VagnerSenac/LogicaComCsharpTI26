f406c6b5486085ccbe9d130dc216ee017db43230
Vagner Lourenço <90052483+VagnerSenac@users.noreply.github.com>
2025-03-26 11:26:57 -0300

    Peso

---
+++ PesoPeixe/PesoPeixe/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PesoPeixe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double peso, sobra;
+            Console.Write("digite o peso pescado: ");
+            peso = int.Parse(Console.ReadLine());
+            sobra = (peso - 50) * 4;
+            Console.Write(sobra);
+        }
+    }
+}
