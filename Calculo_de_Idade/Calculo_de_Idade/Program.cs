a238ab604e162359fba9a157ff1e4aaa10c0aa01
Vagner Lourenço <90052483+VagnerSenac@users.noreply.github.com>
2025-03-26 10:59:10 -0300

    Calculo de Idade
    Primeiro Programa em C#

---
+++ Calculo_de_Idade/Calculo_de_Idade/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculo_de_Idade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int ano_nascimento, ano_atual,idade;
+            //declara as variáveis inteiro
+            Console.Write("Digite o ano de seu nascimento: ");
+            //Console.Write = escreva
+
+            ano_nascimento = int.Parse(Console.ReadLine());
+            //int.Parse - converte texto em numero
+
+            Console.Write("Digite o ano atual: ");
+            ano_atual = int.Parse(Console.ReadLine());
+            idade = ano_atual - ano_nascimento;
+            Console.Write("Sua idade é " + idade + " anos");
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
