fa8eea9a6ce1c677156c1ede8e5f607a9bee529d
Vagner Lourenço <90052483+VagnerSenac@users.noreply.github.com>
2025-04-03 11:06:38 -0300

    cores

--- Numero_Entre_20_90/Numero_Entre_20_90/Program.cs
+++ Numero_Entre_20_90/Numero_Entre_20_90/Program.cs
@@ -8,12 +8,26 @@ namespace Numero_Entre_20_90
 {
     internal class Program
     {
+
         static void Main(string[] args)
         {
-            //Construa um algoritmo que indique se um número digitado está compreendido entre 20 e 90 ou não (20 e 90 não estão na faixa de valores). 
+            
             int numero;
-            Console.Write("Digite um número: ");
+            Console.Title = "SENAC";
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine  ("╔════════════════════════════════════════════════════════════════╗");
+            Console.WriteLine  ("║                                                                ║");
+            Console.WriteLine  ("║ █░█ ▄▀█ █░░ █▀█ █▀█ █▀▀ █▀       █ █▄░█ ▀█▀ █▀▀ █ █▀█ █▀█ █▀   ║"); 
+            Console.WriteLine  ("║ ▀▄▀ █▀█ █▄▄ █▄█ █▀▄ ██▄ ▄█       █ █░▀█ ░█░ ██▄ █ █▀▄ █▄█ ▄█   ║");
+            Console.WriteLine  ("║                                                                ║");
+            Console.WriteLine  ("╚════════════════════════════════════════════════════════════════╝");
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            Console.Write("\r\r Construa um algoritmo que indique se um número digitado está compreendido entre 20 e 90 ou não (20 e 90 não estão na \n faixa de valores).");
+            Console.ResetColor();
+            Console.Write("\n\n Digite um número: ");
             numero = int.Parse(Console.ReadLine());
+            
             if(numero > 20 && numero < 90)
                 // if = SE
                 // && =  e
